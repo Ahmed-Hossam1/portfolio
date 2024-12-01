016e85605204e86a208434f31ef9c7f736642768
Ahmed <medosamsam919@gmail.com>
2024-12-01 10:02:59 +0200

    add animation using Aos Library

--- src/components/Education_Courses/Education_Courses.tsx
+++ src/components/Education_Courses/Education_Courses.tsx
@@ -1,12 +1,20 @@
+import { useEffect } from "react";
 import { Education_CoursesList } from "../data";
 import HeadingText from "../HeadingText";
-
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
 const Education_Courses = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   const Education_CoursesL = Education_CoursesList.map((item, idx) => {
     return (
       <div
         key={idx}
-        className="box  max-w-sm mx-auto md:max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-gray-700 relative before:content[;] before:absolute before:left-0 before:top-0 before:w-0 before:h-full before:bg-gradient-to-r before:from-red-950 before:via-red-900  before:hover:w-full before:transition-all before:duration-300 z-10 before:-z-10"
+        data-aos="fade-up"
+        data-aos-duration="1200"
+        data-aos-delay={idx * 200}
+        className="box  max-w-sm mx-auto md:max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-gray-700 relative before:content[;] before:absolute before:left-0 before:top-0 before:w-0 before:h-full before:bg-gradient-to-r before:from-red-950 before:via-red-900  dark:before:hover:w-full before:transition-all before:duration-300 z-10 before:-z-10"
       >
         <div>
           <h2 className="mb-2  text-2xl font-bold tracking-tight text-gray-900 dark:text-white capitalize ">
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -33,7 +33,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="absolute top-0 left-0 w-full min-h-screen  ">
+      <div className="absolute top-0 left-0 w-full min-h-screen">
         <ParticlesDesign />
       </div>
     </div>
